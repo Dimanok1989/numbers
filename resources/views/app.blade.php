5fcfc983f4acc68ced8353d528d0e9ba69dc61e0
Dimanok1989 <kolgaev@gmail.com>
2022-01-02 22:42:18 +0300

    Git initialisation

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Номера</title>
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/manifest.js') }}" defer></script>
+    <script src="{{ mix('/js/vendor.js') }}" defer></script>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+
+<body>
+
+    <div id="example"></div>
+
+</body>
+
+</html>
