8a229db4fb358e2009594a989da2dfae66e11db7
Dimanok1989 <kolgaev@gmail.com>
2022-01-23 20:27:14 +0300

    Added Ya.Metrika

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,33 @@
         <meta name="keywords" content="@yield('keywords')">
     @endif
 
+    @if (env('APP_ENV') == 'production')
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function(m, e, t, r, i, k, a) {
+                m[i] = m[i] || function() {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                    k, a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym({{ env('YANDEX_METRIKA_ID') }}, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });
+        </script>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/{{ env('YANDEX_METRIKA_ID') }}"
+                    style="position:absolute; left:-9999px;" alt="" /></div>
+        </noscript>
+        <!-- /Yandex.Metrika counter -->
+    @endif
+
     <link href="{{ mix('/css/app.css', mix_path('hot')) }}" rel="stylesheet" />
 </head>
 
